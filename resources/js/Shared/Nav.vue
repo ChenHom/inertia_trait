bede23c336b09617e3bc1176e52d5ad24684b992
Hom Chen <homchen@Homde-MacBook-Pro.local>
2021-11-28 22:33:57 +0800

    add files

---
+++ resources/js/Shared/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="mt-6">
+    <ul class="list-disc">
+      <li><Link href="/home">Home</Link></li>
+      <li><Link href="/users">Users</Link></li>
+      <li><Link href="/settings">Settings</Link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+export default {
+  components: {
+    Link,
+  },
+};
+</script>
+
+<style>
+</style>
