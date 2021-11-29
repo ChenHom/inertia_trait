6146f0ecb9c26a7e3295340e7369f8a81c20a872
hom <hom@macbook.pro>
2021-11-29 23:08:51 +0800

    19 episode completed

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return inertia('Home');
 });
 Route::get('/users', function () {
-    return inertia('Users', [
+    return inertia('Users/Index', [
         'users' => User::query()
             ->when(request('search'), function ($query, $search) {
                 $query->where('name', 'like', '%' . $search . '%');
@@ -27,6 +27,20 @@ Route::get('/users', function () {
             ->withQueryString(),
         'filters' => request()->only(['search'])
     ]);
+});
+Route::post('/users', function () {
+    $attributes = request()->validate([
+        'name' => 'required',
+        'email' => 'required|email',
+        'password' => 'required'
+    ]);
+
+    User::create($attributes);
+
+    return redirect('/users');
+});
+Route::get('/users/create', function () {
+    return inertia('Users/Create');
 });
 Route::get('/settings', function () {
     return inertia('Settings');
